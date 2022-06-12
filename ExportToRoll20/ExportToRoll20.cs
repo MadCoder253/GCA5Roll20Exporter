eec5c823a743e8fe4c93b3102b87879566f45ff6
Ken <kfoubert@outlook.com>
2022-06-11 17:11:13 -0700

    Setting up the initial plugin

---
+++ ExportToRoll20/ExportToRoll20.cs
@@ -0,0 +1,108 @@
+﻿//These aren't usually required Imports within Visual Studio, but have to be included
+//here now because the plugin compiler doesn't make these associations automatically.
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Security;
+
+
+using System.Linq;
+using System.Threading.Tasks;
+using GCA5.Interfaces;
+using GCA5Engine;
+using System.Drawing;
+
+namespace ExportToRoll20
+{
+    public class ExportToRoll20 : GCA5.Interfaces.IExportSheet
+    {
+        private const string PLUGIN_NAME = "Export Character to Roll20";
+        private const string PLUGIN_DESCRIPTION = "Export Character to a json file to import into Roll20. For more informaiton see: https://github.com/MadCoder253/GCA5Roll20Exporter";
+        private const string PLUGIN_VERSION = "1.0.0.0";
+
+        public event IExportSheet.RequestRunSpecificOptionsEventHandler RequestRunSpecificOptions;
+
+        public void CreateOptions(SheetOptionsManager mySheetOptions)
+        {
+            SheetOption mySheetOption = new SheetOption();
+            SheetOptionDisplayFormat myDisplayFormat = new SheetOptionDisplayFormat();
+
+            myDisplayFormat.BackColor = SystemColors.Info;
+            myDisplayFormat.CaptionLocalBackColor = SystemColors.Info;
+
+            mySheetOption.Clear();
+            mySheetOption.Name = "Header_Description";
+            mySheetOption.Type = GCA5Engine.OptionType.Header;
+            mySheetOption.UserPrompt = PluginName() + PluginVersion();
+            mySheetOption.DisplayFormat = myDisplayFormat;
+            mySheetOptions.AddOption(mySheetOption);
+
+            mySheetOption.Clear();
+            mySheetOption.Name = "Description";
+            mySheetOption.Type = GCA5Engine.OptionType.Caption;
+            mySheetOption.UserPrompt = PluginDescription();
+            mySheetOption.DisplayFormat = myDisplayFormat;
+            mySheetOptions.AddOption(mySheetOption);
+        }
+
+        public string PluginDescription()
+        {
+            return PLUGIN_DESCRIPTION;
+        }
+
+        public string PluginName()
+        {
+            return PLUGIN_NAME;
+        }
+
+        public string PluginVersion()
+        {
+            return PLUGIN_VERSION;
+        }
+
+        public int PreferredFilterIndex()
+        {
+            // only json files are supported
+            return 0;
+        }
+
+        public bool PreviewOptions(SheetOptionsManager Options)
+        {
+            // not needed
+            return true;
+        }
+
+        public string SupportedFileTypeFilter()
+        {
+            return "JSON files (*.json)|*.json";
+        }
+
+        public void UpgradeOptions(SheetOptionsManager Options)
+        {
+            //Not needed as of now
+        }
+
+        public bool GenerateExport(Party Party, string TargetFilename, SheetOptionsManager Options)
+        {
+            FileWriter fileWriter = new FileWriter();
+
+            try
+            {
+                fileWriter.FileOpen(TargetFilename);
+
+                fileWriter.Write("{\"title\": \"Export to Roll20\"}");
+            }
+            finally
+            {
+                fileWriter.FileClose();
+            }
+            
+            return true;
+        }
+
+
+    }
+}
---
+++ ExportToRoll20/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Export Character to Roll20")]
+[assembly: AssemblyDescription("Will create a json file that can be used by the Roll20 GURPS Sheet on the Options Import Tab.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Wandering Monsters")]
+[assembly: AssemblyProduct("Export Character to Roll20")]
+[assembly: AssemblyCopyright("Copyright ©  2022")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("39b43a45-f031-40eb-9ffe-96033fd28df5")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
