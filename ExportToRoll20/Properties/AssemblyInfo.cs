4e2ea748559d07ae1166185da5548f4c1c7f2012
Ken <kfoubert@outlook.com>
2023-12-07 21:32:04 -0800

    Update version numbers

--- ExportToRoll20/Properties/AssemblyInfo.cs
+++ ExportToRoll20/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Wandering Monsters")]
 [assembly: AssemblyProduct("Export Character to Roll20")]
-[assembly: AssemblyCopyright("Copyright ©  2022 Wandering Monsters")]
+[assembly: AssemblyCopyright("Copyright ©  2023 Wandering Monsters")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.1.10")]
-[assembly: AssemblyFileVersion("1.0.1.10")]
+[assembly: AssemblyVersion("1.0.1.20")]
+[assembly: AssemblyFileVersion("1.0.1.20")]
